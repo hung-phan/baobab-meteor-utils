6266f147fe4bebefc174e722a8c6e00c300a4baa
hung-phan <colorvisavn@gmail.com>
2016-01-08 00:56:51 +0700

    Update export statement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import autorunBinding from './autorun-binding';
-import cursorListener from './cursor-listener';
-
-export {
-  autorunBinding,
-  cursorListener
-};
+export autorunBinding from './autorun-binding';
+export cursorListener from './cursor-listener';
